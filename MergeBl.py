c25d5942d9b0a5ace2b3271be59a7904b19e42e2
taobody <taobody@gmail.com>
2020-06-12 16:46:42 +0900

    [Fix] 結合したファイル名のフォーマットを修正
    結合後のファイル名が「YYmmdd_HHMMSS_treated.csv」だったのを「YYYYmmdd_HHMMSS_treated.csv」に修正。
    結合開始時間と、終了時間をコンソールに出力するように修正。

--- MergeBl.py
+++ MergeBl.py
@@ -1,45 +1,50 @@
 # -*- coding:utf-8 -*-
 
 import sys
+import datetime
 import pandas as pd
+
+from tqdm import tqdm
 
 
 def merge(enc_path, lidar_path, save_dir):
 
-    # pandasでcvsファイルを読み込んでデータフレームに格納するパターン
+    lidar_enc_df = pd.DataFrame()
+
+    # pandasでcvsファイルを読み込んでデータフレームに格納する
     enc_df = pd.read_csv(enc_path, sep=',', header=None, names=['date', 'Z_axis'])
     lidar_df = pd.read_csv(lidar_path, sep=',', header=None, names=['date', 'X_axis', 'Y_axis'])
 
+    # 結合開始
+    start_time = datetime.datetime.now()
+    print('結合開始：' + start_time.strftime('%Y-%m-%d %H:%M:%S'))
+
+    print('--------------------------------------------')
     # date列を21文字(ミリセカンド1桁)に変更
     # enc_df['date'] = enc_df['date'].str[:21]
     # print(enc_df.head(5))
-    print('--------------------------------------------')
 
     # enc_dfのdate列を19文字（秒）に変更
     enc_df['date'] = enc_df['date'].str[:19]
     enc_df['Z_axis'] = enc_df['Z_axis'] * 100
-    print(enc_df.tail(5))
-    print('--------------------------------------------')
 
     # lidar_dfのdate列を19文字（秒）に変更
     lidar_df['date'] = lidar_df['date'].str[:19]
-    print(lidar_df.tail(5))
-    print('--------------------------------------------')
 
     # 日付データのミリセカンドまでで重複しないもののみ出力
     # print(enc_df.drop_duplicates().info())
 
     # lidar_dfとenc_dfを右外部結合
     lidar_enc_df = pd.merge(lidar_df, enc_df, how="right", on='date')
-    # 再度重複行を削除
+    # # 再度重複行を削除
     lidar_enc_df = lidar_enc_df.drop_duplicates()
-    # 重複しないNaNを含む行を削除
+    # # 重複しないNaNを含む行を削除
     lidar_enc_df = lidar_enc_df.dropna(axis=0, how='any')
-    print(lidar_enc_df)
 
     # 出力（1列目は不要なのでindexはFalseを指定）
-    make_file_name = save_dir + '/treated.csv'
-    print(make_file_name)
-    lidar_enc_df.to_csv(make_file_name, index=False)
-
-    # print(lidar_df['date'].head(3))
+    make_file_name = save_dir + '/' + start_time.strftime('%Y%m%d_%H%M%S') + '_treated.csv'
+    end_time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    print("出力ファイル名：" + make_file_name)
+    print("データ数（行）：" + str(len(lidar_enc_df)))
+    print('--------------------------------------------')
+    print('結合完了：' + end_time)
