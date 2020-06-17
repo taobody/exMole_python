ddae26c99ad69b36f0a2e663f2a5f2cc463f30c7
Motoaki Urayama <taobody@gmail.com>
2020-06-17 13:43:19 +0900

    [Add] 分割用メソッドのテスト

---
+++ SeparateBl.py
@@ -0,0 +1,9 @@
+# -*- coding:utf-8 -*-
+
+
+def seplog(path, pitch, offset, save_dir):
+    print('分割開始')
+    print('パス：', path)
+    print('ピッチ：', pitch, 'cm')
+    print('オフセット：', offset, 'cm')
+    print('保存先：', save_dir)
