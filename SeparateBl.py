f28d0ab158368961ff5d7108c641bd8f3a2825ae
taobody <taobody@gmail.com>
2020-06-23 14:58:42 +0900

    [Fix] オフセットを適用できるように修正

--- SeparateBl.py
+++ SeparateBl.py
@@ -1,15 +1,18 @@
 # -*- coding:utf-8 -*-
+
+import datetime
 import pandas as pd
 
 
 def seplog(path, pitch, offset, save_dir):
-    print('パス：', path)
-    print('ピッチ：', pitch, 'cm')
+    print('ファイル名：', path)
+    print('分割ピッチ：', pitch, 'cm')
     print('オフセット：', offset, 'cm')
     print('保存先：', save_dir)
 
-    # ピッチを㎜に換算
+    # ピッチ、オフセット値を㎜に換算
     pitch = float(pitch) * 10
+    offset = float(offset) * 10
 
     # targetファイルをdataframeに読み込み
     target_df = pd.read_csv(path)
@@ -21,23 +24,26 @@ def seplog(path, pitch, offset, save_dir):
     # データがなくなるまで、ピッチごとのデータを取り出してファイルに書き出し
     # ここで、近似は±50㎜とする
     cnt = 1
-    print('分割開始')
+    start_time = datetime.datetime.now()
+    # print(len(target_df))
+    print('分割開始：' + start_time.strftime('%Y-%m-%d %H:%M:%S'))
     print('--------------------------------------------')
 
     while True:
-        min_pitch = (pitch * cnt) - 50
-        max_pitch = (pitch * cnt) + 50
+        min_pitch = (pitch * cnt) + offset - 50
+        max_pitch = (pitch * cnt) + offset + 50
         separated_df = target_df[(target_df['Z_axis'] > init_dist + min_pitch)
                                  & (target_df['Z_axis'] < init_dist + max_pitch)]
         if not separated_df.empty:
-            make_file_name = save_dir + '/sep_' + str(cnt) + '.csv'
+            make_file_name = save_dir + '/' + start_time.strftime('%Y%m%d_%H%M%S') + '_sep_' + str(cnt) + '.csv'
             separated_df.to_csv(make_file_name, index=False)
             print(make_file_name + 'を作成しました')
             cnt += 1
         else:
             break
-
+            
     print('--------------------------------------------')
-    print('分割終了')
+    end_time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    print('分割完了：' + end_time)
 
 
